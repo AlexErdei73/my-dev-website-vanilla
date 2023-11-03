c39da32b67e9c5a2a322c43f7ed7ee6bd219c707
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-11-03 22:16:38 +0000

    Add backend functions to the app

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 import { showMainElement } from "./components/navigationBar.js";
+import {
+	login,
+	updatePost,
+	updateBlock,
+	getPosts,
+	postPosts,
+	deletePosts,
+	createUser,
+	updateUser,
+	updatePostLikes,
+} from "./backend/backend.js";
+
+let posts = [
+	{
+		title: "...Loading",
+		author: {
+			username: "...Loading",
+		},
+		content: [],
+	},
+];
+
+getPosts().then((json) => {
+	posts = json.posts;
+	console.log(posts);
+});
 
 showMainElement();
