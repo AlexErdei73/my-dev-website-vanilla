b56b1cc0488b992476f0a56fb9fa0e3a3478d236
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-11-06 00:47:19 +0000

    Add prism.js and prism.css for color coding code

--- components/post.js
+++ components/post.js
@@ -1,5 +1,11 @@
+import { escapeHTML } from "../helper.js";
+
 export function initPost(postID) {
   const postNode = document.querySelector(".post");
+
+  const codeNode = postNode.querySelector("code");
+  codeNode.textContent = "<h1>This is a HTML element!</h1>";
+
   const oldNode = postNode.querySelector("div");
   const node = document.createElement("div");
   node.textContent = `Post ID: ${postID}`;
