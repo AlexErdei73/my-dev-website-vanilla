9e8513d511ccd9b9cd057b8437800a8b00fa42cc
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-11-07 11:09:03 +0000

    Remove whitespace from code block template

--- components/block.js
+++ components/block.js
@@ -1,23 +1,23 @@
 import { importTemp } from "../helper.js";
 
 export function initBlock(block) {
-	let blockNode;
-	switch (block.type) {
-		case "paragraph":
-			blockNode = importTemp(8);
-			blockNode.textContent = block.text;
-			break;
-		case "subtitle":
-			blockNode = importTemp(9);
-			blockNode.textContent = block.text;
-			break;
-		case "code":
-			blockNode = importTemp(10);
-			const codeNode = blockNode.querySelector("code");
-			codeNode.textContent = block.text;
-			if (block.language !== " ")
-				codeNode.classList.add(`language-${block.language}`);
-			break;
-	}
-	return blockNode;
+  let blockNode;
+  switch (block.type) {
+    case "paragraph":
+      blockNode = importTemp(8);
+      blockNode.textContent = block.text;
+      break;
+    case "subtitle":
+      blockNode = importTemp(9);
+      blockNode.textContent = block.text;
+      break;
+    case "code":
+      blockNode = importTemp(10);
+      const codeNode = blockNode.querySelector("code");
+      codeNode.textContent = block.text;
+      if (block.language !== " ")
+        codeNode.classList.add(`language-${block.language}`);
+      break;
+  }
+  return blockNode;
 }
