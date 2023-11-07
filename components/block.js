02616b267d2af251e9b3cbd0c82e3215dfcbbd63
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-11-07 21:27:11 +0000

    Add links to paragraph blocks with addLink func

--- components/block.js
+++ components/block.js
@@ -1,11 +1,33 @@
 import { importTemp } from "../helper.js";
 
+function addLinks(text, links) {
+  let shift = 0; //Shift the position from the original with the combined lengths of the insertations
+  links
+    .map((link) => {
+      return {
+        text: `<a href="${link.url}">${link.description}</a>`,
+        pos: link.position,
+      };
+    })
+    .filter((link) => link.pos >= 0) //Filter out links with incorrect position to avoid unwanted behaviour
+    .sort((link1, link2) => link1.pos - link2.pos) //Sort links to increasing position to avoid unwanted behaviour
+    .forEach((link) => {
+      //Insert links in text
+      const position = link.pos + shift;
+      const firstTextPart = text.slice(0, position) + " ";
+      const lastTextPart = " " + text.slice(position, text.length + 1);
+      text = firstTextPart + link.text + lastTextPart;
+      shift = shift + link.text.length + 2;
+    });
+  return text;
+}
+
 export function initBlock(block) {
   let blockNode;
   switch (block.type) {
     case "paragraph":
       blockNode = importTemp(8);
-      blockNode.textContent = block.text;
+      blockNode.innerHTML = addLinks(block.text, block.links);
       break;
     case "subtitle":
       blockNode = importTemp(9);
--- components/post.js
+++ components/post.js
@@ -3,19 +3,19 @@ import { importTemp } from "../helper.js";
 import { prism } from "../prism.js";
 
 export function initPost(post) {
-  const postNode = document.querySelector(".post");
-  const oldNode = postNode.querySelector("div");
-  const node = document.createElement("div");
-  const titleNode = importTemp(7);
-  titleNode.textContent = post.title;
-  node.appendChild(titleNode);
+	const postNode = document.querySelector(".post");
+	const oldNode = postNode.querySelector("div");
+	const node = document.createElement("div");
+	const titleNode = importTemp(7);
+	titleNode.textContent = post.title;
+	node.appendChild(titleNode);
 
-  post.content.forEach((block) => {
-    const blockNode = initBlock(block);
-    node.appendChild(blockNode);
-  });
+	post.content.forEach((block) => {
+		const blockNode = initBlock(block);
+		node.appendChild(blockNode);
+	});
 
-  if (oldNode) postNode.replaceChild(node, oldNode);
-  else postNode.appendChild(node);
-  prism();
+	if (oldNode) postNode.replaceChild(node, oldNode);
+	else postNode.appendChild(node);
+	prism();
 }
