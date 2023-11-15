abd3468b51d8dbce3ce42d925ff1aca6cb5c2a7e
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-11-15 22:31:47 +0000

    Use dialog elements as cards

--- components/modal.js
+++ components/modal.js
@@ -3,41 +3,42 @@ import { importTemp } from "../helper.js";
 let postID = "";
 
 export function initModal({ variant, header, footer, onDelete, onCancel }) {
-  const modalNode = document.querySelector(".modal");
-  const cardNode = importTemp(6);
-  if (variant) cardNode.classList.add(variant);
-  const oldCard = modalNode.querySelector(".card");
-  const cardBody = importTemp(17);
-  const deleteButton = cardBody.querySelector(".delete");
-  deleteButton.addEventListener("click", function () {
-    onDelete(postID);
-  });
-  const cancelButton = cardBody.querySelector(".cancel");
-  cancelButton.addEventListener("click", () => onCancel());
-  const headerNode = cardNode.querySelector(".head .left");
-  headerNode.textContent = header;
-  const footerNode = cardNode.querySelector(".footer .left");
-  footerNode.textContent = footer;
-  const oldBody = cardNode.querySelector(".body");
-  cardNode.replaceChild(cardBody, oldBody);
-  modalNode.replaceChild(cardNode, oldCard);
+	const bodyNode = document.querySelector("body");
+	const cardNode = importTemp(6);
+	cardNode.classList.add("modal");
+	if (variant) cardNode.classList.add(variant);
+	const oldCard = bodyNode.querySelector(".card");
+	const cardBody = importTemp(17);
+	const deleteButton = cardBody.querySelector(".delete");
+	deleteButton.addEventListener("click", function () {
+		onDelete(postID);
+	});
+	const cancelButton = cardBody.querySelector(".cancel");
+	cancelButton.addEventListener("click", () => onCancel());
+	const headerNode = cardNode.querySelector(".head .left");
+	headerNode.textContent = header;
+	const footerNode = cardNode.querySelector(".footer .left");
+	footerNode.textContent = footer;
+	const oldBody = cardNode.querySelector(".body");
+	cardNode.replaceChild(cardBody, oldBody);
+	bodyNode.replaceChild(cardNode, oldCard);
 }
 
 export function showModal(postToDelete = "") {
-  postID = postToDelete;
-  const modalNode = document.querySelector(".modal");
-  modalNode.classList.add("show");
+	postID = postToDelete;
+	const cardNode = document.querySelector("dialog.modal");
+	cardNode.showModal();
 }
 
 export function closeModal() {
-  const modalNode = document.querySelector(".modal");
-  modalNode.classList.remove("show");
+	const cardNode = document.querySelector("dialog.modal");
+	cardNode.close();
 }
 
 export function showErrorMsg(errorMsg) {
-  const cardBody = document.querySelector(".modal .card .body");
-  const msgNode = importTemp(14); //Error Message
-  msgNode.textContent = errorMsg;
-  const pNode = cardBody.querySelector("p");
-  cardBody.replaceChild(msgNode, pNode);
+	const cardBody = document.querySelector(".modal .card .body");
+	const msgNode = importTemp(14); //Error Message
+	msgNode.textContent = errorMsg;
+	const pNode = cardBody.querySelector("p");
+	cardBody.replaceChild(msgNode, pNode);
 }
--- components/postCard.js
+++ components/postCard.js
@@ -3,50 +3,51 @@ import { viewPost, loginData } from "../index.js";
 import { showModal } from "./modal.js";
 
 export function initPost(post, edit) {
-  const postCardNode = importTemp(6);
-  const postCardBody = importTemp(7);
-  const cardBody = postCardNode.querySelector(".body");
-  postCardNode.replaceChild(postCardBody, cardBody);
-  const postTitleNode = postCardNode.querySelector(".body .title h2");
-  const postAuthorNode = postCardNode.querySelector(".head .left");
-  const numberOfLikesNode = postCardNode.querySelector(".head .right");
-  const createdNode = postCardNode.querySelector(".footer .left");
-  const updatedNode = postCardNode.querySelector(".footer .right");
-  const viewButton = postCardNode.querySelector("button.view");
+	const postCardNode = importTemp(6);
+	const postCardBody = importTemp(7);
+	const cardBody = postCardNode.querySelector(".body");
+	postCardNode.replaceChild(postCardBody, cardBody);
+	const postTitleNode = postCardNode.querySelector(".body .title h2");
+	const postAuthorNode = postCardNode.querySelector(".head .left");
+	const numberOfLikesNode = postCardNode.querySelector(".head .right");
+	const createdNode = postCardNode.querySelector(".footer .left");
+	const updatedNode = postCardNode.querySelector(".footer .right");
+	const viewButton = postCardNode.querySelector("button.view");
 
-  postTitleNode.textContent = post.title;
-  postAuthorNode.textContent = `By ${post.author.username}`;
-  numberOfLikesNode.textContent = `Likes: ${post.likes.length}`;
-  createdNode.textContent = `Created: ${post.createdAt.slice(0, 10)}`;
-  updatedNode.textContent = `Updated: ${post.updatedAt.slice(0, 10)}`;
+	postTitleNode.textContent = post.title;
+	postAuthorNode.textContent = `By ${post.author.username}`;
+	numberOfLikesNode.textContent = `Likes: ${post.likes.length}`;
+	createdNode.textContent = `Created: ${post.createdAt.slice(0, 10)}`;
+	updatedNode.textContent = `Updated: ${post.updatedAt.slice(0, 10)}`;
 
-  viewButton.addEventListener("click", function (event) {
-    const buttonNode = event.target;
-    const postID =
-      buttonNode.parentNode.parentNode.parentNode.getAttribute("data-postid");
-    viewPost(postID);
-  });
+	viewButton.addEventListener("click", function (event) {
+		const buttonNode = event.target;
+		const postID =
+			buttonNode.parentNode.parentNode.parentNode.getAttribute("data-postid");
+		viewPost(postID);
+	});
 
-  if (edit) {
-    const publishButton = postCardNode.querySelector("button.publish");
-    const deleteButton = postCardNode.querySelector("button.delete");
-    deleteButton.addEventListener("click", function (event) {
-      const buttonNode = event.target;
-      const postID =
-        buttonNode.parentNode.parentNode.parentNode.getAttribute("data-postid");
-      showModal(postID);
-    });
-    publishButton.classList.remove("hidden");
-    publishButton.textContent = post.published ? "Hide" : "Publish";
-    deleteButton.classList.remove("hidden");
-  }
+	if (edit) {
+		const publishButton = postCardNode.querySelector("button.publish");
+		const deleteButton = postCardNode.querySelector("button.delete");
+		deleteButton.addEventListener("click", function (event) {
+			const buttonNode = event.target;
+			const postID =
+				buttonNode.parentNode.parentNode.parentNode.getAttribute("data-postid");
+			showModal(postID);
+		});
+		publishButton.classList.remove("hidden");
+		publishButton.textContent = post.published ? "Hide" : "Publish";
+		deleteButton.classList.remove("hidden");
+	}
 
-  if (!edit && loginData.success) {
-    const likeButton = postCardNode.querySelector("button.like");
-    likeButton.textContent =
-      post.likes.indexOf(loginData.user._id) === -1 ? "Like" : "Unlike";
-    likeButton.classList.remove("hidden");
-  }
+	if (!edit && loginData.success) {
+		const likeButton = postCardNode.querySelector("button.like");
+		likeButton.textContent =
+			post.likes.indexOf(loginData.user._id) === -1 ? "Like" : "Unlike";
+		likeButton.classList.remove("hidden");
+	}
 
-  return postCardNode;
+	postCardNode.show();
+	return postCardNode;
 }
