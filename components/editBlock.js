38972030a6395e97fb05aab73aa1c648f4fdacbe
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-11-19 15:16:47 +0000

    Make cancel button work in edit block

--- components/editBlock.js
+++ components/editBlock.js
@@ -1,7 +1,13 @@
 import { importTemp } from "../helper.js";
+import { initBlock } from "./block.js";
 
 export function initEditBlock(block) {
   const editBlockNode = importTemp(20);
+  const cancelButton = editBlockNode.querySelector(".buttons .cancel");
+  cancelButton.addEventListener("click", function () {
+    const blockNode = initBlock(block, true);
+    editBlockNode.parentNode.replaceChild(blockNode, editBlockNode);
+  });
   const blockNode = document.querySelector(
     `.block[data-blockid="${block._id}"]`
   );
