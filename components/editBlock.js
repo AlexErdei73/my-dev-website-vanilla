49434f4fc1fa89d9dbf84c1b35fe680bfa4846ad
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-11-25 15:39:27 +0000

    Add getNewBlock function to editBlock.js

--- components/editBlock.js
+++ components/editBlock.js
@@ -24,13 +24,19 @@ function textWithLinks(block) {
 export function initEditBlock(block) {
   const editBlockNode = importTemp(20);
   const cancelButton = editBlockNode.querySelector(".buttons .cancel");
-  const inputNode = editBlockNode.querySelector(".input-container input");
+  const inputNode = importTemp(21);
+  const textareaNode = editBlockNode.querySelector(".input-container textarea");
+  const selectTypeNode = editBlockNode.querySelector("select#type");
+  selectTypeNode.value = block.type;
   if (block.type !== "subtitle") {
-    const textareaNode = editBlockNode.querySelector(
-      ".input-container textarea"
-    );
-    inputNode.classList.toggle("hidden");
-    textareaNode.classList.toggle("hidden");
+    inputNode.addEventListener("change", function () {
+      console.log("Input Change!");
+      textareaNode.value = inputNode.value;
+    });
+    textareaNode.addEventListener("change", function () {
+      console.log("Textarea Change!");
+      inputNode.value = textareaNode.value;
+    });
     textareaNode.style = "";
     if (block.type === "code") {
       const selectLangNode = editBlockNode.querySelector("#language");
@@ -42,14 +48,59 @@ export function initEditBlock(block) {
     }
   } else {
     inputNode.value = block.text;
+    textareaNode.parentNode.replaceChild(inputNode, textareaNode);
   }
   cancelButton.addEventListener("click", function () {
     const blockNode = initBlock(block, true);
     editBlockNode.parentNode.replaceChild(blockNode, editBlockNode);
     if (block.type === "code") prism();
   });
+  editBlockNode.addEventListener("submit", function (event) {
+    event.preventDefault();
+    console.log(getNewBlock(block, editBlockNode));
+  });
   const blockNode = document.querySelector(
     `.block[data-blockid="${block._id}"]`
   );
   blockNode.parentNode.replaceChild(editBlockNode, blockNode);
+}
+
+function getNewBlock(block, node) {
+  const inputNode = node.querySelector("input");
+  const textareaNode = node.querySelector("textarea");
+  const selectTypeNode = node.querySelector("select#type");
+  const selectLangNode = node.querySelector("select#language");
+  const newBlock = { ...block };
+  newBlock.type = selectTypeNode.value;
+  if (newBlock.type !== "subtitle") newBlock.text = textareaNode.value;
+  else newBlock.text = inputNode.value;
+  newBlock.language = " ";
+  if (newBlock.type === "code") newBlock.language = selectLangNode.value;
+  return newBlock;
+}
+
+function separateLinksFromText(textInput) {
+  const textPieces = textInput.split("](");
+  const links = [];
+  let position = 0;
+  textPieces.forEach((piece, index) => {
+    const previousPiece = textPieces[index - 1];
+    if (previousPiece && previousPiece.indexOf("[") !== -1) {
+      const pieces = previousPiece.split("[");
+      textPieces[index - 1] = pieces[0];
+      position += pieces[0].length;
+      links.push({ description: pieces[1] });
+    }
+    if (piece.indexOf(")") !== -1) {
+      const pieces = piece.split(")");
+      const link = links.pop();
+      if (link) {
+        link.url = pieces[0];
+        link.position = position;
+        links.push(link);
+        textPieces[index] = pieces[1];
+      }
+    }
+  });
+  return { links: links, text: textPieces.join("") };
 }
